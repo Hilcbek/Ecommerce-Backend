3f9604f1140302e755a63044a593537da30c233e
Hilcbek <balemayehu07@gmail.com>
2024-03-05 19:55:39 +0300

    backend added 2

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "https://ecommerce-app-iota-green.vercel.app",
-      "http://localhost:5173",
-    ],
+    origin: ["https://habesha-market-hub.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
